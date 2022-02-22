d8be35f4dbbeccef7ffa30e46ce229dd1bb2b343
Shubhamaggarwal11051987 <98207517+Shubhamaggarwal11051987@users.noreply.github.com>
2022-02-22 19:20:35 +0530

    AKASH mvc crud

--- AKASH.MVC_1/Controllers/EmployeeController.cs
+++ AKASH.MVC_1/Controllers/EmployeeController.cs
@@ -11,7 +11,7 @@ namespace AKASH.MVC_1.Controllers
     public class EmployeeController : Controller
     {
         private ApplicationDbContext dbContext;
-        public EmployeeController(ApplicationDbContext dbcontext) 
+        public EmployeeController(ApplicationDbContext dbcontext)
         {
             this.dbContext = dbcontext;
         }
@@ -27,9 +27,44 @@ namespace AKASH.MVC_1.Controllers
         [HttpPost]
         public IActionResult Create(Employee emp)
         {
+            if (ModelState.IsValid) { 
             dbContext.Employees.Add(emp);
             dbContext.SaveChanges();
             return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(emp);
+            }
         }
-    }
+        public IActionResult Delete(int Id)
+
+        {
+            var emp = dbContext.Employees.SingleOrDefault
+                (e => e.Id == Id);
+            if (emp != null)
+            {
+                dbContext.Employees.Remove(emp);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public IActionResult Edit(int Id)
+        {
+            var emp = dbContext.Employees.SingleOrDefault(e => e.Id == Id);
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            dbContext.Employees.Update(emp);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
 }
+    }
+
--- AKASH.MVC_1/Models/Employee.cs
+++ AKASH.MVC_1/Models/Employee.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Xunit;
+using Xunit.Sdk;
 
 namespace AKASH.MVC_1.Models
 {
@@ -10,9 +12,26 @@ namespace AKASH.MVC_1.Models
     {
         
         public int Id { get; set; }
+        [Display(Name = "Employee Name")]
+        [Required(ErrorMessage ="Employee Name can't Empty!")]
+        [MinLength(3,ErrorMessage ="Name must be min 3 char!")]
+        [MaxLength(30,ErrorMessage ="Name must be max 30 char!")]
         public string Name { get; set; }
+
+        [Display(Name = "Employee Email Address")]
+        [Required(ErrorMessage ="Employee Email can't Empty!")]
+        [EmailAddress(ErrorMessage="Email address is not valid!")]
         public string Email { get; set; }
+
+        [Display(Name = "Employee Mobile")]
+        [Required(ErrorMessage ="Employee Mobile can't Empty!")]
+        [RegularExpression("^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$",
+            ErrorMessage="Invalid mobile number !")]
         public string Mobile { get; set; }
+        [Display(Name = "Employee Address")]
+        [Required]
+        [MaxLength(100,ErrorMessage="max 200 Char !")]
         public string Address { get; set; }
+       
     }
 }
